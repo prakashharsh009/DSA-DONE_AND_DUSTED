fb93869b474c397e800bc76a41793ecd7b415b40
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2022-10-16 22:07:06 +0530

    Update L42. Floor in a Binary Search Tree.cpp

--- Binary Tree 🌳/Striver's Tree Playlist/L42. Floor in a Binary Search Tree.cpp
+++ Binary Tree 🌳/Striver's Tree Playlist/L42. Floor in a Binary Search Tree.cpp
@@ -1,1 +1,19 @@
-
+int floorInBST(TreeNode<int> * root, int X)
+{
+    // Write your code here.
+    int floor  = -1;
+    while (root){
+        if(root->val == X){
+            floor = root->val;
+            return floor;
+        }
+        else if(root->val < X){
+            floor = root->val;
+            root = root->right;
+        }
+        else{
+            root = root->left;
+        }
+    }
+   return floor;
+}
