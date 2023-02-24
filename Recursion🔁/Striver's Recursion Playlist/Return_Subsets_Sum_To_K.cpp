9027a224d0cc9d28629e398d20a7207e7fac66b4
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-02-24 13:31:25 +0530

    Update Return_Subsets_Sum_To_K.cpp

--- Recursion🔁/Striver's Recursion Playlist/Return_Subsets_Sum_To_K.cpp
+++ Recursion🔁/Striver's Recursion Playlist/Return_Subsets_Sum_To_K.cpp
@@ -1,1 +1,20 @@
+void subsets(vector<int> &arr, vector<int> vec, vector<vector<int>> &res , int index , int n , int sum, int k){
 
+    if(index == n){
+      if (sum == k) {
+        res.push_back(vec);
+      }
+      return;
+    }
+    //Take and Not-Take Approach
+
+    vec.push_back(arr[index]);
+    sum += arr[index];
+
+    subsets(arr,vec, res, index+1, n, sum, k);
+
+    vec.pop_back();
+    sum -= arr[index];
+
+    subsets(arr, vec, res, index+1, n, sum, k);
+}
