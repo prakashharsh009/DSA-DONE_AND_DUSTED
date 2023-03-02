5f121c7e7f3d31d16917bafa3d8d22c703d71286
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-03-02 21:53:13 +0530

    Create K-th Permutation Sequence.cpp

---
+++ Recursion🔁/Striver's Recursion Playlist/K-th Permutation Sequence.cpp
@@ -0,0 +1,16 @@
+/***************************************************** APPROACH 1 - BRUTE FORCE (recursive) *****************************************************/
+
+
+string kthPermutation(int n, int k) {
+    // Write your code here.
+  
+  string input = "";
+  
+  for (int i = 1 ; i <= n ; i++ {
+    char ch = i + 48
+    input += ch;
+  }
+       
+  vector<string> res;
+   
+
