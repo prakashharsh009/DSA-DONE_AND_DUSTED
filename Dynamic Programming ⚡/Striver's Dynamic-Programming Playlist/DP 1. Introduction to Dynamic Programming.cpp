f2dbf0566ad612cb0bc9447d00cea047ce5c98c4
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-03-05 18:50:28 +0530

    Update DP 1. Introduction to Dynamic Programming.cpp

--- Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP 1. Introduction to Dynamic Programming.cpp
+++ Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP 1. Introduction to Dynamic Programming.cpp
@@ -3,7 +3,7 @@ Link - https://takeuforward.org/data-structure/dynamic-programming-introduction/
 
 EXAMPLE = FIBONACCI NUMBER
 
-Recursive Approach
+Recursive Approach - Top to Bottom 
 
 int fib(int n){
 
@@ -25,3 +25,39 @@ if(dp[n] != -1){
   }
 return dp[n] = fib(n-1) + fib(n-2);
 }
+
+Time Complexity = O(N)
+Space Complexity = O(N)
+Auxiliary Space = O(N)
+  
+  
+Tabulation Approach - Bottom Up 
+
+int fib(int n , vector<int> &dp){
+  dp[0] = 0;
+  dp[1] = 1;
+  
+  for (int i = 2 ; i < n ; i++){
+    dp[i] = dp[i-1] + dp[i-2];
+  }
+return dp[n];
+}
+Time Complexity = O(N)
+Space Complexity = O(N) (No recursion call stack)
+  
+  
+  BEST APPROACH
+  
+int fib(int n ){
+
+if(n == 0 or n == 1) return n;
+int n1 = 0 , n2 = 1;
+int n3;
+for (int i = 2 ; i < n ; i++){
+    n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
+ }
+ return n3;
+}
+
