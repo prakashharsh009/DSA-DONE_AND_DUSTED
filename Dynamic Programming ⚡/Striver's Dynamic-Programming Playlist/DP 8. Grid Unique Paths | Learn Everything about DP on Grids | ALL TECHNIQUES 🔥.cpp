7c5ef54c78832990550e510a0ac96848029710f0
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-03-27 21:53:46 +0530

    Update DP 8. Grid Unique Paths | Learn Everything about DP on Grids | ALL TECHNIQUES 🔥.cpp

--- Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP 8. Grid Unique Paths | Learn Everything about DP on Grids | ALL TECHNIQUES 🔥.cpp
+++ Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP 8. Grid Unique Paths | Learn Everything about DP on Grids | ALL TECHNIQUES 🔥.cpp
@@ -31,3 +31,42 @@ int memo(int i , int j , vector<vector<int>> &dp){
 }
 
 //DYNAMIC PROGRAMMING - TABULATION
+int tab(int m , int n){
+
+	vector<vector<int>> dp(m, vector<int>(n,-1));
+
+	for (int i = 0  ; i < m ; i++){
+		for (int j = 0 ; j < n ; j++){
+			if(i == 0 and j == 0) dp[i][j] = 1;
+			else{
+				int up = 0 ,  left = 0;
+				if(i > 0) up = dp[i-1][j];
+				if(j > 0) left = dp[i][j-1];
+
+				dp[i][j] = up + left;
+			}	
+		}
+	}
+	return dp[m-1][n-1];
+}
+
+
+//SPACE OPTIMISED
+int space(int m , int n){
+
+	vector<int> prev(n, -1);
+	for (int i  = 0 ; i < m ; i++){
+		vector<int> temp(n, -1);
+		for (int j = 0 ; j < n ; j++){
+			if( i == 0 and j == 0) temp[j]  = 1;
+			else{
+				int up = 0 , left = 0;
+				if(i > 0) up = prev[j];
+				if(j > 0) left = temp[j-1];
+			 	temp[j] = up + left;
+			}
+		}
+		prev = temp;
+	}
+	return prev[n-1];
+}
