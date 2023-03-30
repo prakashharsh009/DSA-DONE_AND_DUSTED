75dae8475b6796aa89156c7b053723e7d67d063f
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-03-30 16:09:31 +0530

    Update DP-11. Triangle | Fixed Starting Point and Variable Ending Point | DP on GRIDS.cpp

--- Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP-11. Triangle | Fixed Starting Point and Variable Ending Point | DP on GRIDS.cpp
+++ Dynamic Programming ⚡/Striver's Dynamic-Programming Playlist/DP-11. Triangle | Fixed Starting Point and Variable Ending Point | DP on GRIDS.cpp
@@ -24,3 +24,44 @@ int memo(int i , int j, int n, vector<vector<int>> &triangle,vector<vector<int>>
 }
 
 //DYNAMIC PROGRAMMING - TABULATION
+int tab( int n, vector<vector<int>> &triangle){
+
+	vector<vector<int>> dp(n, vector<int>(n,-1));
+
+	//Since starting from the last row therefore filling the last row of DP with repective values from traingle 2D
+	for (int j = 0 ; j < n ; j++){
+		dp[n-1][j] = triangle[n-1][j];
+	}
+	for (int i = n-2 ; i >= 0 ; i--){
+		for (int j = 0 ; j < i + 1 ; j++){
+			int down = triangle[i][j] + dp[i+1][j];
+			int diag = triangle[i][j] + dp[i+1][j+1];
+			dp[i][j] = min(down,diag);
+		}
+	
+	}
+	return dp[0][0];
+}
+
+//SPACE OPTIMISED
+int space(int n, vector<vector<int>> &triangle){
+
+	vector<int> prev(n,0);
+
+	//Since starting from the last row therefore filling the first instance of prev with repective values from traingle 2D
+	for (int j = 0 ; j < n ; j++){
+		prev[j] = triangle[n-1][j];
+	}
+	for (int i = n-2 ; i >= 0 ; i--){
+		vector<int> temp(n,0);
+		for (int j = 0 ; j < i + 1 ; j++){
+
+			int down = triangle[i][j] + prev[j];
+			int diag = triangle[i][j] + prev[j+1];
+
+			temp[j] = min(down,diag);
+		}
+		prev = temp;
+	}
+	return prev[0];
+}
