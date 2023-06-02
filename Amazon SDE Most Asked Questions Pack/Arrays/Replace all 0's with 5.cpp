25daf2fa1c5378ed14c13d6de207774a817ee265
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-06-02 19:08:30 +0530

    Update Replace all 0's with 5.cpp

--- Amazon SDE Most Asked Questions Pack/Arrays/Replace all 0's with 5.cpp
+++ Amazon SDE Most Asked Questions Pack/Arrays/Replace all 0's with 5.cpp
@@ -1,1 +1,20 @@
-
+int convertFive(int num) {
+    // code here
+    int res = 0;
+    
+    while (num){
+        if(num%10){
+            res = res *10 + num%10;
+        }
+        else{
+            res = res * 10 + 5;
+        }
+        num /= 10;
+    }
+    int ans = 0;
+    while(res){
+        ans  =  ans *10 + res % 10;
+        res /= 10;
+    }
+    return ans;
+}
