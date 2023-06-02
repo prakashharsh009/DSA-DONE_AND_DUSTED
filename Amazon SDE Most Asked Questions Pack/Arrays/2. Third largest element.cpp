90af3594faf0daffacc2e23b814b47e11b50550f
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-06-02 19:20:04 +0530

    Update 2. Third largest element.cpp

--- Amazon SDE Most Asked Questions Pack/Arrays/2. Third largest element.cpp
+++ Amazon SDE Most Asked Questions Pack/Arrays/2. Third largest element.cpp
@@ -1,1 +1,26 @@
-
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         //Your code here
+         int finalres = INT_MIN;
+         
+         int first_Max = INT_MIN;
+         for (int i = 0 ; i < n ; i++){
+             first_Max = max(a[i],first_Max);
+         }
+         int second_Max = INT_MIN;
+         for (int i  =  0; i < n ; i++){
+             if(a[i] != first_Max){
+                 second_Max = max(second_Max,a[i]);
+             }
+         }
+         
+         for (int i  = 0 ; i < n ; i++){
+             if(a[i] != first_Max and a[i] != second_Max){
+                 finalres  = max(finalres, a[i]);
+         }
+    }
+    return finalres;
+}
+};
