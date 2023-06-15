a8a4db7b7fc2866b5cf2b7efdae908bbaf634364
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-06-15 22:51:53 +0530

    Update 5. Rotate Array.cpp

--- Amazon SDE Most Asked Questions Pack/Arrays/5. Rotate Array.cpp
+++ Amazon SDE Most Asked Questions Pack/Arrays/5. Rotate Array.cpp
@@ -1,1 +1,11 @@
-
+void rotateArr(int arr[], int d, int n){
+    // code here
+    
+    while ( d > n){
+        d -= n;
+    }
+    reverse(arr, arr+d);
+    reverse(arr+d , arr+ n);
+    reverse(arr, arr+n);
+  
+}
