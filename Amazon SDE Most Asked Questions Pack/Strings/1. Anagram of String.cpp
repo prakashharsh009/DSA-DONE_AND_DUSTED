081952f1713a9fb513eb4a0c73acb57826a95404
Harsh Prakash <70228834+prakashharsh009@users.noreply.github.com>
2023-07-05 14:01:15 +0530

    Update 1. Anagram of String.cpp

--- Amazon SDE Most Asked Questions Pack/Strings/1. Anagram of String.cpp
+++ Amazon SDE Most Asked Questions Pack/Strings/1. Anagram of String.cpp
@@ -24,3 +24,25 @@
 
 /--------------------------------------------------------------------- NO. OF OPERATIONS FOR ANAGRAM ---------------------------------------------------------------------/
 
+int remAnagram(string str1, string str2)
+{
+  
+    // Your code goes here
+    int count1[26] = {0}; 
+    int count2[26] = {0};
+    
+    for (int i  = 0 ; i < str1.size(); i++){
+        count1[str1[i] - 'a']++;
+    }
+     
+    for (int i  = 0 ; i < str2.size(); i++){
+        count2[str2[i] - 'a']++;
+    }
+    
+    int result  = 0;
+    
+    for (int i = 0 ; i < 26; i++){
+        result += abs(count1[i] - count2[i]);
+    }
+    return result;
+}
